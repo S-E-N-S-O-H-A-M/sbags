17e9f55cbf4a08bd02a8a00282df94d2fc22d51c
SOHAM SEN <sohamsen2542000@gmail.com>
2020-05-30 09:59:34 +0530

    Updated Url

--- astromania/urls.py
+++ astromania/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from .import views
 
 urlpatterns=[path('home',views.homepage),path('login',views.login),path('log_ch',views.log_check),path('reg',views.reg),path('register',views.register),path('about',views.about),path('contact',views.contact),path('recipes',views.recipes),path('Tandoori',views.Tandoori),]
